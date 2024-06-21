1a8c6a0d883243196ac2dd4bb6711917b5f601f6
sakthi <sakthisvsp01@gmail.com>
2024-06-21 17:26:32 +0530

    changes

--- server.js
+++ server.js
@@ -3,27 +3,26 @@ const app = express()
 const socketIo = require('socket.io')
 const server = require('http').createServer(app);
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const io = socketIo(server, {
     cors: {
         origin: 'http://localhost:3000'
     }
 })
 
-const users = []
+const users = new Map()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.post('/login', (req, res) => {
     try {
         const { username } = req.body
-        if (!username) {
-            return res.status(400).json({ error: 'Username is required' })
-        }
+        // if (!username) {
+        //     return res.status(400).json({ error: 'Username is required' })
+        // }
         const userId = Math.random().toString(36)
-        users.push({
-            userId: userId,
-            username: username
-        })
-        res.status(200).json({ userId, username })
+        users.set(userId, username)
+        res.json({ userId, username })
     } catch (error) {
         console.error('Login error:', error)
         res.status(500).json({ error: 'Internal server error' })
@@ -31,8 +30,9 @@ app.post('/login', (req, res) => {
 })
 
 io.on('connection', (socket) => {
+    
     socket.on('join', ({ userId }) => {
-        const username = users.find((user) => user.userId === userId)
+        const username = users.get(userId)
         if (username) {
             socket.username = username
             console.log(`User ${username} joined with ID: ${socket.id}`)
@@ -42,18 +42,20 @@ io.on('connection', (socket) => {
             })
         }
     })
+    
+    console.log('client connected:', socket.username)
     socket.on('chatMessage', (msg) => {
         console.log('Message received:', msg)
         io.emit('message', {
-            id: `${socket.id}`,
+            id:`${socket.id}`,
             username: socket.username,
-            message: msg
+            message:msg
         })
     })
 
     socket.on('disconnect', (reason) => {
         console.log(reason)
-        io.emit('message', `User disconnected: ${socket.id}`)
+        io.emit('message', `User disconnected: ${socket.username}`)
     })
 })
 
